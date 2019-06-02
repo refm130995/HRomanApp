b11dec8ebc2eeee0927b481b514c0ee0421ea21e
Ramon Figuera <refm.130995@gmail.com>
2019-06-01 21:03:05 -0400

    as

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
 import {
-  NavController,
-  LoadingController,
-  AlertController
+  Component
+} from '@angular/core';
+import {
+  NavController, LoadingController, AlertController
 } from 'ionic-angular';
-import { ServicesProvider } from '../../providers/services/services';
-import { Storage } from '@ionic/storage';
+import {
+  ServicesProvider
+} from '../../providers/services/services';
+import {
+  Storage
+} from '@ionic/storage';
 import { Step2formPage } from '../step2form/step2form';
 import { ListMasterPage } from '../list-master/list-master';
 import { User } from '../../providers/user/user';
@@ -17,46 +21,36 @@ import { User } from '../../providers/user/user';
 export class HomePage {
   users: any = [];
   role: any;
-  choferes: any;
   form: any = {
-    elemntos_de_desgaste_cambiados: [
+    elemntos_de_desgaste_cambiados : [
       {
-        descripcion: 'Cuchillas',
-        cantidad: 0
+        'descripcion': '',
+        'cantidad' : 0
       },
       {
-        descripcion: 'Punteras',
-        cantidad: 0
+        'descripcion': '',
+        'cantidad' : 0
       },
       {
-        descripcion: 'Bronces',
-        cantidad: 0
+        'descripcion': '',
+        'cantidad' : 0
       },
       {
-        descripcion: 'Fenólicas',
-        cantidad: 0
+        'descripcion': '',
+        'cantidad' : 0
       },
       {
-        descripcion: 'Insertos',
-        cantidad: 0
-      },
-      {
-        descripcion: 'Calzas',
-        cantidad: 0
+        'descripcion': '',
+        'cantidad' : 0
       }
     ],
     aceites: [],
-    filtros: []
+    filtros:[]
   };
   exist: boolean;
-  constructor(
-    public navCtrl: NavController,
-    public service: ServicesProvider,
-    private storage: Storage,
-    private loadingCtrl: LoadingController,
-    private alertCtrl: AlertController
-  ) {
+  constructor(public navCtrl: NavController, public service: ServicesProvider, private storage: Storage, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
     console.log(this.form);
+    
   }
 
   ionViewDidLoad() {
@@ -68,107 +62,82 @@ export class HomePage {
       content: 'Consultando Rol...'
     });
     loading.present();
-    this.storage.get('User').then(result => {
+    this.storage.get('User').then((result) => {
       let role = result.role;
       loading.dismiss();
       if (role != 'Empleado') {
         loading.dismiss();
         this.role = result['role'];
-        this.service.ListByRol(result.token).subscribe(
-          resp => {
+        this.service.ListByRol(result.token).subscribe((resp) => {
             this.users = resp['users'];
             console.log(this.users);
+            
           },
-          error => {
+          (error) => {
             console.log(error);
-          }
-        );
+          })
       } else {
-        var user;
-        this.storage.get('User').then(data => {
-          this.form.nombre_del_ejecutor = data.firstname + ' ' + data.lastname;
-          this.form.mantencion_dentro_mm50h = 0;
-          this.service.ListChofer(data.token).subscribe(
-            resp => {
-              loading.dismiss();
-              this.choferes = resp['choferes'];
-            },
-            error => {
-              loading.dismiss();
-              console.log(error);
-            }
-          );
-        });
+        loading.dismiss();
         this.role = 'Empleado';
       }
-    });
+    })
   }
 
-  calculate_mantencion() {
-    this.form.mantencion_dentro_mm50h =
-      this.form.horometro_programado - this.form.horometro_real;
+  next(){
+    this.navCtrl.push(Step2formPage,{
+      form:this.form
+    })
   }
 
-  next() {
-    this.navCtrl.push(Step2formPage, {
-      form: this.form
-    });
-  }
-
-  check_(data: string) {
+  check_(data:string){
     this.exist = false;
-    let index = this.form.aceites.indexOf(data);
-    if (index >= 0) {
-      this.form.aceites.splice(index, 1);
-    } else {
-      this.form.aceites.push(data);
-    }
+  let index = this.form.aceites.indexOf(data);
+      if(index >= 0){
+        this.form.aceites.splice(index, 1);
+      }else{
+        this.form.aceites.push(data);
+      }
+
   }
-  check_2(data: string) {
+  check_2(data:string){
     this.exist = false;
     let index = this.form.filtros.indexOf(data);
-    if (index >= 0) {
-      this.form.filtros.splice(index, 1);
-    } else {
-      this.form.filtros.push(data);
-    }
+      if(index >= 0){
+        this.form.filtros.splice(index, 1);
+      }else{
+        this.form.filtros.push(data);
+      }
+  
   }
 
-  showInform(user_: any) {
+  showInform(user_:any){
     this.navCtrl.push(ListMasterPage, {
       user: user_
     });
   }
-
-  deleteUser(user: any) {
+  
+  deleteUser(user:any){
     let loading = this.loadingCtrl.create({
       content: 'Eliminando...'
     });
     loading.present();
-    this.storage.get('User').then(result => {
-      this.service.DeleteUser(user, result.token).subscribe(
-        resp => {
-          loading.dismiss();
-          this.GetUsers();
-        },
-        error => {
-          loading.dismiss();
-          console.log(error);
-        }
-      );
-    });
+    this.storage.get('User').then((result) => {
+    this.service.DeleteUser(user, result.token).subscribe((resp) => {
+      loading.dismiss();
+      this.GetUsers();
+    },
+    (error) => {
+      loading.dismiss();
+      console.log(error);
+    })
+  })
   }
 
-  AlertDelete(user_) {
-    console.log(user_);
+  AlertDelete(user_){
+    console.log(user_)
     let alert = this.alertCtrl.create({
       title: 'Alerta!!',
-      message:
-        '¿Seguro que desea eliminar a ' +
-        user_.firstname +
-        ' ' +
-        user_.lastname +
-        ' ?',
+      message: '¿Seguro que desea eliminar a '+ user_.firstname + ' ' +user_.lastname+ ' ?',
       buttons: [
         {
           text: 'Cancelar',
@@ -180,7 +149,7 @@ export class HomePage {
         {
           text: 'Eliminar',
           handler: () => {
-            this.deleteUser(user_);
+           this.deleteUser(user_);
           }
         }
       ]
--- src/pages/step2form/step2form.ts
+++ src/pages/step2form/step2form.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  IonicPage,
-  NavController,
-  NavParams,
-  AlertController,
-  LoadingController,
-  ToastController
-} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, ToastController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { ServicesProvider } from '../../providers/services/services';
 import { Storage } from '@ionic/storage';
@@ -15,30 +8,21 @@ import { HomePage } from '../home/home';
 @IonicPage()
 @Component({
   selector: 'page-step2form',
-  templateUrl: 'step2form.html'
+  templateUrl: 'step2form.html',
 })
 export class Step2formPage {
   base64Image: string;
   form: any = [];
   users: any = {};
 
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    private alertCtrl: AlertController,
-    private camera: Camera,
-    private service: ServicesProvider,
-    private storage: Storage,
-    private loadingCtrl: LoadingController,
-    private toastCtrl: ToastController
-  ) {
-    this.form = this.navParams.get('form');
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, private camera: Camera, private service: ServicesProvider, private storage: Storage, private loadingCtrl: LoadingController, private toastCtrl: ToastController) {
+  this.form = this.navParams.get('form');
   }
 
   ionViewDidLoad() {
-    this.alert();
+   this.alert();
   }
-  alert() {
+  alert(){
     let alert = this.alertCtrl.create({
       message: '¿Desea sacar fotografia?',
       buttons: [
@@ -52,27 +36,26 @@ export class Step2formPage {
         {
           text: 'Si',
           handler: () => {
-            this.pickPhoto();
+           this.pickPhoto();
           }
         }
       ]
     });
     alert.present();
   }
-  pickPhoto() {
+  pickPhoto(){
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
-    };
-
-    this.camera.getPicture(options).then(
-      imageData => {
-        this.base64Image = 'data:image/jpeg;base64,' + imageData;
-      },
-      err => {}
-    );
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     
+    });
   }
 
   save() {
@@ -84,53 +67,50 @@ export class Step2formPage {
     this.form.version = '5';
     this.form.tabla_final = [
       {
-        version: '1',
-        descripcion: 'incorporación al sistema documentado',
-        fecha_aprobacion: '03/06/2013',
-        elabora_aprueba: 'Mauricio Mendez/Héctor Román'
+          "version": "1",
+          "descripcion": "incorporación al sistema documentado",
+          "fecha_aprobacion": "03/06/2013",
+          "elabora_aprueba": "Mauricio Mendez/Héctor Román"
       },
       {
-        version: '2',
-        descripcion: 'incorporación al sistema documentado',
-        fecha_aprobacion: '03/06/2013',
-        elabora_aprueba: 'Mauricio Mendez/Héctor Román'
+          "version": "2",
+          "descripcion": "incorporación al sistema documentado",
+          "fecha_aprobacion": "03/06/2013",
+          "elabora_aprueba": "Mauricio Mendez/Héctor Román"
       }
-    ];
-    this.form.saca_fotografia = 'No';
-    this.form.fecha_revision = '13/03/2019';
-    this.form.elaborado_por = 'Mauricio Mendez';
-    this.form.revisado_por = 'Millaray Rovira';
-    this.form.aprobado_por = 'Héctor Román';
-    this.storage
-      .get('User')
-      .then(result => {
-        this.form.user = result['_id'];
-        if (this.form.sin_personal_disp) {
-          this.form.sin_personal_disp = 'Si';
-        } else {
-          this.form.sin_personal_disp = 'No';
-        }
-        this.service.SaveInform(this.form, result.token).subscribe(
-          resp => {
-            loading.dismiss();
-            let toast = this.toastCtrl.create({
-              message: 'Informe enviado Exitosamente!!',
-              duration: 3000,
-              position: 'top'
-            });
-            toast.present();
-            this.navCtrl.setRoot(HomePage);
-          },
-          error => {
-            console.log(error);
-            loading.dismiss();
-          }
-        );
-      })
-      .catch(err => {});
+  ];
+  this.form.saca_fotografia = 'No';
+  this.form.fecha_revision = '13/03/2019';
+  this.form.elaborado_por = 'Mauricio Mendez';
+  this.form.revisado_por = 'Millaray Rovira';
+  this.form.aprobado_por = 'Héctor Román';
+  this.storage.get('User').then((result) => {
+    this.form.user = result['_id'];
+    if(this.form.sin_personal_disp){
+      this.form.sin_personal_disp = 'Si'
+    }else{
+      this.form.sin_personal_disp = 'No'
+    }
+    this.service.SaveInform(this.form, result.token).subscribe((resp) => {
+     loading.dismiss();
+     let toast = this.toastCtrl.create({
+      message: 'Informe enviado Exitosamente!!',
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+    this.navCtrl.setRoot(HomePage);
+    },
+    (error) => {
+      console.log(error);
+      loading.dismiss();
+    })
+  }).catch((err) => {
+    
+  });
   }
-  AlertDelete(user_) {
-    console.log(user_);
+  AlertDelete(user_){
+    console.log(user_)
     let alert = this.alertCtrl.create({
       title: 'Alerta!!',
       message: '¿Seguro que desea enviar este informe?',
@@ -145,41 +125,11 @@ export class Step2formPage {
         {
           text: 'Enviar',
           handler: () => {
-            this.save();
+           this.save();
           }
         }
       ]
     });
     alert.present();
-  }
-
-  calculate() {
-    var ht = this.toDate(this.form.hora_termino || '00:00');
-    var hi = this.toDate(this.form.hora_icinio || '00:00');
-    if (this.form.hora_termino !== '' && this.form.hora_icinio !== '') {
-      this.form.total_horas = this.parseMillisecondsIntoReadableTime(
-        ht.getTime() - hi.getTime()
-      );
-    }
-  }
-  toDate(dStr) {
-    var now = new Date();
-    now.setHours(dStr.substr(0, dStr.indexOf(':')));
-    now.setMinutes(dStr.substr(dStr.indexOf(':') + 1));
-    now.setSeconds(0);
-    return now;
-  }
-  parseMillisecondsIntoReadableTime(milliseconds) {
-    //Get hours from milliseconds
-    var hours = milliseconds / (1000 * 60 * 60);
-    var absoluteHours = Math.floor(hours);
-    var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
-
-    //Get remainder from hours and convert to minutes
-    var minutes = (hours - absoluteHours) * 60;
-    var absoluteMinutes = Math.floor(minutes);
-    var m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
-
-    return h + ':' + m;
   }
 }
